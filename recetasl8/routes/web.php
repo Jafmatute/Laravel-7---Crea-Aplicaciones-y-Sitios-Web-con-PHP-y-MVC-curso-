8c2b2e953b1e786043bbccbd51b38838505fc727
Jafmatute <Jafmatute@gmail.com>
2020-11-09 03:53:17 -0600

    Create Login and Authentication

--- recetasl8/routes/web.php
+++ recetasl8/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RecetaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/recetas', [RecetaController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
